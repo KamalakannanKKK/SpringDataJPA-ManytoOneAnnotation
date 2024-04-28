d877a14d16facef3097fbf0a8fae74bee81f1038
KamalakannanKKK <109393676+KamalakannanKKK@users.noreply.github.com>
2024-04-28 14:13:06 +0530

    Create ManytoOneMappingApplicationTests.java

---
+++ src/test/java/com/example/ManytoOneMapping/ManytoOneMappingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ManytoOneMapping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManytoOneMappingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
